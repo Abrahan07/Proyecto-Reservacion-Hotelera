f1fd6289c4ad391a1fd78e29c9dd58ae25cc20c8
Abrahan Remolina Bolivar <abrahanremolina703@gmail.com>
2026-05-10 20:09:21 -0500

    feat: estructura del proyecto base

---
+++ src/main/java/com/universidad/staytic/StayticApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.staytic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universidad/staytic/StayticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.staytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
